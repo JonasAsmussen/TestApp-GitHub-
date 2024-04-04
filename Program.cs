58aa9644c26dc58f6f7e43545a0c0751f71b7153
Jonas Asmussen <Jonas Asmussen@MSI>
2024-04-04 09:44:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("Mojn Lømler");
+
+Console.WriteLine("Virker denne test? (Svar med en Console.Writeline()");
